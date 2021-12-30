3d4cf26f4ec1ef3e8cc42aa5bdce0eb92f407b83
vaperyy <91438529+vaperyy@users.noreply.github.com>
2021-12-29 17:28:14 -0800

    Beta, buggy background scrolling

---
+++ Assets/Scripts/ScrollScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float scrollSpeed = -5f;
+    Vector2 startPos;
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, 11);
+        transform.position = startPos + Vector2.up * newPos;
+    }
+}
