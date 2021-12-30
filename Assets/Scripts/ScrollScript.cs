12a3fbfd9cbe8c3ac0553bc136e60b1491cd109b
vaperyy <91438529+vaperyy@users.noreply.github.com>
2021-12-29 19:35:44 -0800

    FULLY WORKING PARALLAX!

--- Assets/Scripts/ScrollScript.cs
+++ Assets/Scripts/ScrollScript.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+https://www.youtube.com/watch?v=IgZQjGyB9zg&list=PLxI_bfnfwf5ISnH12_Sx-0Ucei_O9QJIO&index=7
+
+and comment from: https://www.youtube.com/watch?v=zit45k6CUMk&t
+
+
+*/
 public class ScrollScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    float scrollSpeed = -3f;
+    public float scrollSpeed = -3f;
     Vector2 startPos;
     void Start()
     {
