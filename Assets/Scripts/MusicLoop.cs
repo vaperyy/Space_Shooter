0ba48691899703618c5bd2dde74a5437e81f20fa
vaperyy <91438529+vaperyy@users.noreply.github.com>
2022-01-12 12:12:25 -0800

    Update MusicLoop with annotation

--- Assets/Scripts/MusicLoop.cs
+++ Assets/Scripts/MusicLoop.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MusicLoop : MonoBehaviour
 {
+    /*
+    https://www.youtube.com/watch?v=li2D_PEdST4
+    */
     public AudioSource musicSource;
     // Start is called before the first frame update
     void Start()
