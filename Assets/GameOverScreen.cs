1f1f0b4ed836a61887c059c09e0ed2addad18b80
vaperyy <jingjl23@uci.edu>
2022-08-05 01:22:35 -0700

    Game Over Screen

---
+++ Assets/GameOverScreen.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;  // you have to specifically import UI to use Text
+
+public class GameOverScreen : MonoBehaviour
+{
+    // public Text pointsText;
+    // public void Setup(int score) 
+    // {
+    //     gameObject.SetActive(true);
+    //     pointsText.text = score.ToString() + " POINTS";
+    // }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,13 @@ https://www.youtube.com/watch?v=tFblCEFQoTs
 
 */
 {
+
+    public GameOverScreen GameOverScreen;  // game over screen from that class
+    int maxPlatform = 0;  // also new
+
+
+
+
     public Projectile laserPrefab;
     public float moveSpeed = 5.0f;
     public Rigidbody2D rb;  // adding a Rigidbody assigns physics properties to the sprite
@@ -37,6 +44,25 @@ https://www.youtube.com/watch?v=tFblCEFQoTs
         // this.transform.position locates the GameObject in 3d world space
         // Quaternion.identity represents no rotation needed, object is perfectly aligned with world axises
         Instantiate(this.laserPrefab, this.transform.position, Quaternion.identity);
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    /* 
+    Just like in projectile except for the shooter and an enemy plane
+
+    NOTE: OnTriggerEnter2D only works once you implement a box collider for objects involved, 
+    as well as check the checkbox for trigger
+    */
+    
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("EnemyPlane")) 
+        {
+            // GameOverScreen.Setup(maxPlatform);
+            // Destroy(other.gameObject);
+        }
+        // Destroy(this.gameObject);
     }
     
 }
